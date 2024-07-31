a072703cfcefb06a2709d13799197cf42654138d
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-07-31 21:09:50 +0530

    :sparkles: navbar updated.

---
+++ components/Navbar.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+
+export default function Navbar() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <nav className="border-b p-4 flex items-center justify-between">
+      <h1 className="text-3xl font-bold">
+        <Link href="/">Stock Management System</Link>
+      </h1>
+      {loggedIn ? (
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>
+              <Button className="rounded" variant={"destructive"}>
+                Logout <LogOut className="ml-2"/>
+              </Button>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ) : (
+        <Button className="rounded">
+          <Link href="/login">Login / Sign Up</Link>
+        </Button>
+      )}
+    </nav>
+  );
+}
